fc10d34a058078e2795e9a9576a740b374b27e9b
Somed-1 <abbasovmaqa159@gmail.com>
2024-01-25 13:48:22 +0400

    Fix everything

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    check = lambda x: ord(x) >= ord("a") and ord(x) <= ord("z")
-    for i in str:
-        a = chr(ord(i) - 32) if check(i) else i
-        print("".format(a), end="")
-    print()
+    result = ''
+    for let in str:
+        asci_let = ord(let)
+        if (asci_let >= 97 and asci_let <= 122):
+            up_let = chr(asci_let - 32)
+            result += up_let
+        else:
+            result += chr(asci_let)
+    print("{}".format(result))
