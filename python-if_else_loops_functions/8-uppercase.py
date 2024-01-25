0ef0fe64b119a47e71a764c2202e44afdc8f56c1
Somed-1 <abbasovmaqa159@gmail.com>
2024-01-25 13:46:42 +0400

    Fix format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     check = lambda x: ord(x) >= ord("a") and ord(x) <= ord("z")
     for i in str:
         a = chr(ord(i) - 32) if check(i) else i
-        print(a, end="")
+        print("".format(a), end="")
     print()
