d6c222f59cf3d207b0eb42d504b18234c1743fb6
Somed-1 <abbasovamaqa159@gmail.com>
2024-01-29 11:18:24 +0400

    Fix copy

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = matrix[:]
+    new = matrix.copy()
     for i in new:
         for j in range(len(i)):
             i[j] = i[j] ** 2
