adea5ba17c9f91521554c2b8eadbb5eac0bd6791
Somed-1 <abbasovamaqa159@gmail.com>
2024-01-30 17:56:10 +0400

    Fix 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dicitionary):
-    return max(a_dicitionary.items())[0] if a_dicitionary else None
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
+    return max(a_dicitionary, key=a_dicitionary.get) if a_dicitionary else None
