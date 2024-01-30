1d60f5c2860c2c4c330b12d5c466c94d0b1a2656
Somed-1 <abbasovamaqa159@gmail.com>
2024-01-30 17:54:21 +0400

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dicitionary):
-    return max(a_dicitionary.values())
+    return max(a_dicitionary.items())[0] if a_dicitionary else None
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
