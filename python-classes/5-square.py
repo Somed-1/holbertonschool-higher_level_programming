8dd77107929ca371d028f25385aa5a2f7fafbeca
Somed-1 <abbasovamaqa159@gmail.com>
2024-01-31 13:17:48 +0400

    Task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Define a class Square with properities"""
+
+
+class Square:
+    """Square class."""
+
+    def __init__(self, size=0) -> None:
+        """__init__ method that sets the size of the square.
+        Args:
+            size (int): size of the square.
+        """
+        self.size = size
+
+    def area(self):
+        """Public instance method that returns the
+        current square area.area method that returns the area of the square.
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Public instance method that pritns the current square.
+        """
+        for i in range(self.size):
+            print("{}".format("#" * self.size))
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
