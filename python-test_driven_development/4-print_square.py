27026a4ba8ff45a88380020c64900e506a4febcb
Somed-1 <abbasovmaqa159@gmail.com>
2024-02-02 17:54:20 +0400

    Task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains the function taht prints square"""
+
+
+def print_square(size):
+    """
+    print_square - prints square usign #
+    Args:
+        size: the size of square (int)
+    Return:
+        Nothing
+    """
+    if not isinstance(size, (int)):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        print()
+
+    for row in range(size):
+        print("{}".format("#" * size))
