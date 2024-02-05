b3b496cbe4b0a7b90043e9c1a70951113d856311
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-05 14:03:13 +0400

    Task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This module contains class Rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle"""
+    def __init__(self, width=0, height=0) -> None:
+        """
+        __init__ - method to inicialize an instance of class
+        Args:
+            width: width of Rectangle
+            height: height of Rectangle
+        Return:
+            nothing
+        """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """
+        area - return area of Rectangle
+        Return:
+            area of Rectangle
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+        perimeter - returns perimeter of Rectangle
+        Return:
+            perimeter of Rectangle
+        """
+        return self.width * 2 + self.height * 2
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
