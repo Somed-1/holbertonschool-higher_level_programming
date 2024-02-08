9394f4d41ce3e7e0803494ef69c36b2e5780f153
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-08 15:04:39 +0400

    Fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,4 @@
 def inherits_from(obj, a_class):
     """inhertis_from - returns if is an instance of a class"""
 
-    return isinstance(obj, a_class)
+    return False if type(obj) is a_class else isinstance(obj, a_class)
