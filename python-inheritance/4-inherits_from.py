dace6f4f936aa482b3ffa9bbd026f66935908fbf
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-08 14:53:29 +0400

    Task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains a function inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """inhertis_from - returns if is an instance of a class"""
+
+    return isinstance(obj, a_class)
