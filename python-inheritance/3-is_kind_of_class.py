c7d45d04e65940a90f5130e391b3a1bed8e192b8
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-08 14:50:11 +0400

    Fix

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -7,5 +7,4 @@ This module contains a function is_kind_of_class
 def is_kind_of_class(obj, a_class):
     """is_kind_of_class - returns if obj is a subclass of a_class"""
 
-
     return issubclass(type(obj), a_class)
