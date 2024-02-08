0cdc70a2a6bca01ec3fc471948450eb65bf3e415
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-08 11:49:34 +0400

    Returns new list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,4 +6,5 @@ class MyList(list):
     """MyList class"""
     def print_sorted(self):
         """print_sorted method"""
-        print(sorted(self))
+        new_list = sorted(self)
+        print(new_list)
