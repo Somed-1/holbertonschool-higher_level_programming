48ca758f4e4ca98a66e092b22adb5da5cb8e8ad9
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 18:43:17 +0400

    Fix task 100

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(count=[0]):
     count[0] += 1
-    return ["BestSchool, " if i + 1 != count[0] else "BestSchool" for i in range(count[0])]
+    return ", ".join("BestSchool" for i in range(count[0]))
