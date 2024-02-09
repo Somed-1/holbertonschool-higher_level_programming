8b7935a2ac9a3311099402f5509ba41efe95c493
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 18:54:43 +0400

    Fix documentation task 101

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -6,6 +6,8 @@ class LockedClass:
     """LockedClass class"""
 
     def __setattr__(self, __name: str, __value: Any) -> None:
+        """__setattr__ method"""
+
         if __name == "first_name":
             super().__setattr__(__name, __value)
         raise AttributeError(f"'LockedClass' object has no attribute '{__name}'")
