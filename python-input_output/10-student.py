fac55f35e017517c4585d41053273e610b97dc1c
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 14:01:44 +0400

    Fix task 10 pycodestyle

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,6 +13,6 @@ class Student:
     def to_json(self, keys=None):
         if keys is not None:
             if all(isinstance(i, str) for i in keys):
-                n = {key: getattr(self, key) for key in keys if hasattr(self, key)}
+                n = {k: getattr(self, k) for k in keys if hasattr(self, k)}
                 return n
         return self.__dict__
