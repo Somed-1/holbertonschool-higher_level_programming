edc799194f9de5de0db64a9125fc6d1819518293
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 18:06:19 +0400

    Task 101

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains add_attribute function"""
+
+
+def add_attribute(instance, name, value):
+    """add_attribute function"""
+
+    setattr(instance, name, value)
