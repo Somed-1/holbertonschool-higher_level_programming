1815f642251dc4d6bdef20706f49bd76572c5a8d
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 19:17:44 +0400

    fix

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -11,7 +11,8 @@ class LockedClass:
         if __name == "first_name":
             super().__setattr__(__name, __value)
         else:
-            raise AttributeError(f"'LockedClass' object has no attribute '{__name}'")
+            text = f"'LockedClass' object has no attribute '{__name}'"
+            raise AttributeError(text)
 
     def __getattribute__(self, __name):
         """__getattribute__ method"""
@@ -19,4 +20,5 @@ class LockedClass:
         if __name == "first_name":
             super().__getattribute__(__name)
         else:
-            raise AttributeError(f"'LockedClass' object has no attribute '{__name}'")
+            text = f"'LockedClass' object has no attribute '{__name}'"
+            raise AttributeError(text)
