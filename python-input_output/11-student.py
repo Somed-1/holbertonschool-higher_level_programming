70bf9caaa7edc925b93d6927bc1ef88c64d47a6f
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 14:05:24 +0400

    Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains class Student"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, keys=None):
+        if keys is not None:
+            if all(isinstance(i, str) for i in keys):
+                n = {k: getattr(self, k) for k in keys if hasattr(self, k)}
+                return n
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dict__ = json
