f8125b1f5ee5f87599b73c53200876bd8ca4128b
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 18:53:48 +0400

    Task 101

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains LockedClass class"""
+
+
+class LockedClass:
+    """LockedClass class"""
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == "first_name":
+            super().__setattr__(__name, __value)
+        raise AttributeError(f"'LockedClass' object has no attribute '{__name}'")
