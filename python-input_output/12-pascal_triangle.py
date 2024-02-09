d6cb45d29b8afc2a0ab96bfd2204a42d56230aa2
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 14:42:19 +0400

    Fix task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,7 +6,7 @@ def pascal_triangle(n):
     """pascal_triangle function"""
 
     rows = [[1]]
-    for j in range(n):
+    for j in range(n - 1):
         above = [0] + rows[-1] + [0]
         row = [above[i] + above[i + 1] for i in range(len(above) - 1)]
         rows.append(row)
