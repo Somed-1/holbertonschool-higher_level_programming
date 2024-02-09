214abbbf866059f85592a1162c74e918b4172d8f
Somed-1 <abbasovamaqa159@gmail.com>
2024-02-09 15:47:35 +0400

    Fix taks 100

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -8,8 +8,9 @@ def append_after(filename="", search_string="", new_string=""):
     lines = []
     with open(filename, "r") as file:
         for line in file:
-            line = line.replace(search_string, new_string)
             lines.append(line)
+            if search_string in line:
+                lines.append(new_string)
 
     with open(filename, "w") as file:
         file.writelines(lines)
