b641a24da8bc90bdfe4c520ff000fe55d6242240
Somed-1 <abbasovmaqa159@gmail.com>
2024-02-12 01:50:13 +0400

    Task 2 class rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""This module contains class Rectangle."""
+
+
+class Rectangle:
+    """class Rectangle"""
+
+    def __init__(self, width, height, x, y) -> None:
+        """__init__ method of class Rectangle"""
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be integer")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("heihgt must be integer")
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("x must be integer")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("y must be integer")
+        self.__y = value
