c344c3eb846a85c3f40b39fb803903a28a77164c
Somed-1 <abbasovmaqa159@gmail.com>
2024-02-12 01:23:35 +0400

    Fix incorrct bahavior

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,7 +8,28 @@ class Base:
     __nb_objects = 0
     def __init__(self, id=None) -> None:
         if id is None:
-            self.__nb_objects += 1
+            self.increase_nb()
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @classmethod
+    def increase_nb(cls):
+        cls.__nb_objects += 1
+
+
+if __name__ == "__main__":
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
