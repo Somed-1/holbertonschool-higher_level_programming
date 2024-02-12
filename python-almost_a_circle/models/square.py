a15698688c36ce2a6dd092efbfd84e275c539997
Somed-1 <abbasovmaqa159@gmail.com>
2024-02-12 15:52:11 +0400

    New file square.py that contains Square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module contains class Square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """__init__ method of class Square"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self) -> str:
+        """__str__ method of class Square"""
+        rectangle_str = super().__str__()
+        return rectangle_str.replace("Rectangle", "Square")
