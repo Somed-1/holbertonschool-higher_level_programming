c102a826ea940ee96b798b7b1c091102dc6c093f
Somed-1 <abbasovmaqa159@gmail.com>
2024-03-24 18:35:32 +0400

    Task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT
+    s.title,
+    g.genre_id
+FROM
+    tv_show_genres g
+    LEFT JOIN tv_shows s ON s.id = g.show_id
+ORDER BY
+    s.title,
+    g.genre_id;
